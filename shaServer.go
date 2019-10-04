3cb23c7f35e92b377153acaa2a0e5403ebd71ba7
EC2 Default User <ec2-user@ip-10-0-0-242.us-west-1.compute.internal>
2019-10-04 22:11:21 +0000

    add url params to configure sha count and string

--- shaServer.go
+++ shaServer.go
@@ -5,14 +5,36 @@ import (
   "fmt"
   "log"
   "net/http"
+  "strconv"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  sum := sha256.Sum256([]byte("this is a hash\n"))
-  fmt.Fprintf(w, "Calculated SHA: %v\n", sum)
+  keys, ok := r.URL.Query()["key"]
+  if !ok || len(keys) < 1 {
+    log.Println("Url Param missing")
+   return
+  }
+  counts, ok := r.URL.Query()["count"]
+  if !ok || len(counts) < 1 {
+    log.Println("Url Param missing")
+   return
+  }
+  key := keys[0]
+  count, err := strconv.Atoi(counts[0])
+  if err != nil {
+    log.Println("count should be an integer")
+  }
+  sum := sha256.Sum256([]byte(key))
+  for i := 1; i < count; i++ {
+    sum = sha256.Sum256([]byte(key))
+    log.Println("Calculated SHA for %v: %v",key, sum)
+  }
+  fmt.Fprintf(w, "<html><body>Calculated SHA for %v: %v\n</body></html>",key, sum)
 }
-
+ 
+// We'll calculate shas to busy the cpu.  We can vary the filesize and
+// the number of times we calculate sha given url params
 func main() {
   http.HandleFunc("/", handler)
-  log.Fatal(http.ListenAndServe(":8080", nil))
+  log.Fatal(http.ListenAndServe(":80", nil))
 }
